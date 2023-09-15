25a592be6842993aa07a9a2ee4264f263e3873e4
Samit Koyom <samitkoyom@gmail.com>
2023-09-15 20:45:29 +0700

    update requirement setting 1

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # แก้ไขบรรทัดนี้สำหรับ production
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
